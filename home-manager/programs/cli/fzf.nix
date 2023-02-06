49a4b764cbabab7af42d967f2d0618e136384591
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-06 23:48:13 +0100

    fix: remove extra config for fzf
    resolves #4

--- home-manager/programs/cli/fzf.nix
+++ home-manager/programs/cli/fzf.nix
@@ -9,9 +9,6 @@
       "--height 40%"
       "--layout=reverse"
       "--border"
-      "--color=bg+:#313244,bg:#1e1e2e,spinner:#f5e0dc,hl:#f38ba8"
-      "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
-      "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
     ];
   };
 }
