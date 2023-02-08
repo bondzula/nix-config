7d04b01608367acb41ce038b5b2b787ffd1d5bfc
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-09 00:20:14 +0100

    fix(shell): adjust the starship prompt
    remove the noise

--- home-manager/programs/cli/starship.nix
+++ home-manager/programs/cli/starship.nix
@@ -1,8 +1,22 @@
-{ config, pkgs, ... }:
+{ config, pkgs, lib, ... }:
 
 {
   programs.starship = {
     enable = true;
     enableZshIntegration = true;
+    settings = {
+      format = lib.concatStrings [
+        "$username"
+        "$hostname"
+        "$directory"
+        "$git_branch"
+        "$git_commit"
+        "$git_state"
+        "$git_metrics"
+        "$git_status"
+        "$package"
+        "nix_shell"
+      ];
+    };
   };
 }
