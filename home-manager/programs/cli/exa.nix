8a9f015132785aa48aae9331d65a3fdbf1bd69d7
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-15 10:54:11 +0100

    refactor(exa): set aliases manualy

--- home-manager/programs/cli/exa.nix
+++ home-manager/programs/cli/exa.nix
@@ -1,8 +1,14 @@
-{ config, pkgs, ... }:
+# User configuration for exa
+
+{ pkgs, ... }:
 
 {
-  programs.exa = {
-    enable = true;
-    enableAliases = true;
-  };
+  # Install exa
+  home.packages = [ pkgs.exa ];
+
+  # Aliases
+  programs.zsh.initExtra = ''
+    ealias ls="exa --all --group-directories-first --across"
+    ealias ll="exa --all --group-directories-first --long"
+  '';
 }
