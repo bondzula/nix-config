983b10c35353e940a7b2ce6c7937c757fbb13513
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-02-16 23:54:21 +0100

    refactor(chromium): adjust extensions

--- modules/home-manager/chromium.nix
+++ modules/home-manager/chromium.nix
@@ -1,19 +1,19 @@
-{ pkgs, ... }:
+_:
 
 {
   programs.chromium = {
     enable = true;
     extensions = [
       # uBlock Origin
-      { id = "cjpalhdlnbpafiamejdnhcphjbkeiagm"; }
+      {
+        id = "cjpalhdlnbpafiamejdnhcphjbkeiagm";
+      }
       # Vue Dev Tools
-      { id = "nhdogjmejiglipccpnnnanhbledajbpd"; }
+      {
+        id = "nhdogjmejiglipccpnnnanhbledajbpd";
+      }
       # KeePassXC-Browser
       { id = "oboonakemofpalcgghocfoadofidjkkk"; }
-      # JSON Formatter
-      { id = "bcjindcccaagfpapjjmafapmmgkkhgoa"; }
-      # LanguageTool
-      { id = "oldceeleldhonbafppcapldpdifcinji"; }
     ];
   };
 }
