4ec639b2ceca4fbdb29513a0c918e014cec439d2
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-03-01 22:17:20 +0100

    feat(ssh): add matchBlock for github.com

--- modules/home-manager/ssh.nix
+++ modules/home-manager/ssh.nix
@@ -6,5 +6,15 @@
 
     # Keep SSH sessions alive by sending, every minute, a keep-alive signal to hosts
     serverAliveInterval = 60;
+
+    # Per host settings
+    matchBlocks = {
+      # GitHub
+      "github.com" = {
+        hostname = "github.com";
+        user = "git";
+        identityFile = "/home/bondzula/.ssh/github";
+      };
+    };
   };
 }
