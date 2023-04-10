f9c667f5e83ff5ec59512fd09b94a5b5fee0b514
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-04-10 17:51:21 +0200

    feat: improve wezterm config

---
+++ home-manager/modules/desktop/wezterm/default.nix
@@ -0,0 +1,8 @@
+{ pkgs, ... }:
+
+{
+  # Install the package
+  home.packages = with pkgs; [ wezterm ];
+
+  xdg.configFile."wezterm/wezterm.lua".source = ./wezterm.lua;
+}
---
+++ home-manager/modules/desktop/wezterm/wezterm.lua
@@ -0,0 +1,119 @@
+-- Pull in the wezterm API
+local wezterm = require 'wezterm'
+local act = wezterm.action
+
+-- This table will hold the configuration.
+local config = {}
+
+-- In newer versions of wezterm, use the config_builder which will
+-- help provide clearer error messages
+if wezterm.config_builder then
+  config = wezterm.config_builder()
+end
+
+config.font     	= wezterm.font("JetBrains Mono")
+config.font_size	= 14.0
+
+-- For example, changing the color scheme:
+config.color_scheme = 'Catppuccin Mocha'
+
+-- Appearance
+config.window_background_opacity = 0.9
+config.window_decorations = "NONE"
+
+-- TODO:
+--  - new window
+--  - new window with a command (lazygit, lf)
+--  - new workspace
+--  - switch between windows
+--  - switch between workspaces
+-- Keybinds
+config.leader = { key = 'Space', mods = 'CTRL', timeout_milliseconds = 1000 }
+
+config.keys = {
+  -- splits
+  {
+    key = 'v',
+    mods = 'LEADER',
+    action = wezterm.action.SplitHorizontal { domain = 'CurrentPaneDomain' },
+  },
+  {
+    key = 's',
+    mods = 'LEADER',
+    action = wezterm.action.SplitVertical { domain = 'CurrentPaneDomain' },
+  },
+  -- pane navigation
+  {
+    key = 'h',
+    mods = 'LEADER',
+    action = act.ActivatePaneDirection('Left'),
+  },
+  {
+    key = 'l',
+    mods = 'LEADER',
+    action = act.ActivatePaneDirection('Right'),
+  },
+  {
+    key = 'k',
+    mods = 'LEADER',
+    action = act.ActivatePaneDirection('Up'),
+  },
+  {
+    key = 'j',
+    mods = 'LEADER',
+    action = act.ActivatePaneDirection('Down'),
+  },
+  {
+    key = 'q',
+    mods = 'LEADER',
+    action = act.CloseCurrentPane({ confirm = true }),
+  },
+  -- window naviation
+  {
+    key = '1',
+    mods = 'ALT',
+    action = act.ActivateTab(0),
+  },
+  {
+    key = '2',
+    mods = 'ALT',
+    action = act.ActivateTab(1),
+  },
+  {
+    key = '3',
+    mods = 'ALT',
+    action = act.ActivateTab(2),
+  },
+  {
+    key = '4',
+    mods = 'ALT',
+    action = act.ActivateTab(3),
+  },
+  {
+    key = '5',
+    mods = 'ALT',
+    action = act.ActivateTab(4),
+  },
+  {
+    key = 'n',
+    mods = 'LEADER',
+    action = act.ActivateTabRelative(1),
+  },
+  {
+    key = 'p',
+    mods = 'LEADER',
+    action = act.ActivateTabRelative(-1),
+  },
+  {
+    key = 'g',
+    mods = 'LEADER',
+    action = act.SpawnCommandInNewTab({
+      args = { 'lazygit' }
+    }),
+  },
+  -- Rotate panes
+  -- https://wezfurlong.org/wezterm/config/lua/keyassignment/RotatePanes.html
+
+}
+
+return config
