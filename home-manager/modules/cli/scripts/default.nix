01b5897968754340030277897e9084159639aca0
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-07-30 23:05:02 +0200

    feat(zellij): add session(tab) managment scripts

--- home-manager/modules/cli/scripts/default.nix
+++ home-manager/modules/cli/scripts/default.nix
@@ -11,6 +11,18 @@
     executable = true;
   };
 
+  # Script to manage Tmux window names (icons)
+  xdg.configFile."bin/zellij-zoxide" = {
+    source = ./zellij-zoxide;
+    executable = true;
+  };
+
+  # Script to manage Tmux window names (icons)
+  xdg.configFile."bin/zs" = {
+    source = ./zellij-session;
+    executable = true;
+  };
+
   # Add ~/config/bin to PATH
   programs.zsh.initExtra = ''
     path+=('/home/bondzula/.config/bin')
