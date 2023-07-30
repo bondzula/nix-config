3675421da2c535169ab6b5fd9457b3700c77c743
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-07-30 22:35:07 +0200

    fix(fzf): use home-manager option for default opts

--- home-manager/modules/cli/fzf.nix
+++ home-manager/modules/cli/fzf.nix
@@ -14,14 +14,13 @@
     defaultCommand = "command fd --hidden --type file --exclude .git";
     # Default command for the CTRL+T key binding
     fileWidgetCommand = "${config.programs.fzf.defaultCommand}";
+    # Default settings
+    defaultOptions = [
+      "--color=spinner:#f5e0dc,hl:#f38ba8"
+      "--color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc"
+      "--color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8"
+      "--min-height=100 --reverse"
+      "--no-separator"
+    ];
   };
-
-  programs.zsh.envExtra = ''
-    export FZF_DEFAULT_OPTS=" \
-      --color=spinner:#f5e0dc,hl:#f38ba8 \
-      --color=fg:#cdd6f4,header:#f38ba8,info:#cba6f7,pointer:#f5e0dc \
-      --color=marker:#f5e0dc,fg+:#cdd6f4,prompt:#cba6f7,hl+:#f38ba8 \
-      --min-height=100 --reverse \
-      --no-separator"
-  '';
 }
