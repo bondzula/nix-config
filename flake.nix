672a4ea8622ff73ea4341683282d746f415e4971
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-07-31 21:36:23 +0200

    feat(flake): refactor, and simplify main flake

--- flake.nix
+++ flake.nix
@@ -1,65 +1,45 @@
 {
   inputs = {
-    # Nixpkgs
-    nixpkgs = { url = "github:nixos/nixpkgs/nixos-unstable"; };
+    nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
+    hardware.url = "github:NixOS/nixos-hardware/master";
+    devenv.url = "github:cachix/devenv/latest";
 
-    # Load hardware config for laptop
-    hardware = { url = "github:NixOS/nixos-hardware/master"; };
-
-    # Flake helpers
-    flake-parts = { url = "github:hercules-ci/flake-parts"; };
-
-    # For now only used for firefox plugins
-    nur = { url = "github:nix-community/NUR"; };
-
-    # Home manager
     home-manager = {
       url = "github:nix-community/home-manager";
       inputs.nixpkgs.follows = "nixpkgs";
     };
-
-    devenv = { url = "github:cachix/devenv/latest"; };
-
-    neovim-nightly-overlay = { url = "github:nix-community/neovim-nightly-overlay"; };
   };
 
-  outputs = inputs@{ nixpkgs, flake-parts, home-manager, nur, neovim-nightly-overlay, ... }:
-
-    flake-parts.lib.mkFlake { inherit inputs; } {
-      systems = [ "x86_64-linux" "aarch64-darwin" ];
-
-      flake = {
-        nixosConfigurations = {
-          "G14" = nixpkgs.lib.nixosSystem {
-            modules = [ ./nixos/G14 ];
-            specialArgs = { inherit inputs; };
-          };
-          "apollo" = nixpkgs.lib.nixosSystem {
-            modules = [ ./nixos/apollo ];
-            specialArgs = { inherit inputs; };
-          };
-        };
-
-        homeConfigurations = {
-          "bondzula@apollo" = home-manager.lib.homeManagerConfiguration {
-            pkgs = import nixpkgs {
-              system = "x86_64-linux";
-              overlays = [ nur.overlay neovim-nightly-overlay.overlay ];
-              config.allowUnfree = true;
-            };
-            extraSpecialArgs = { inherit inputs; };
-            modules = [ ./home-manager/apollo.nix ];
-          };
-          "bondzula" = home-manager.lib.homeManagerConfiguration {
-            pkgs = import nixpkgs {
-              system = "x86_64-linux";
-              overlays = [ nur.overlay neovim-nightly-overlay.overlay ];
-              config.allowUnfree = true;
-            };
-            extraSpecialArgs = { inherit inputs; };
-            modules = [ ./home-manager/g14.nix ];
-          };
-        };
+  outputs = { nixpkgs, home-manager, ... }@inputs: {
+    nixosConfigurations = {
+      "G14" = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./nixos/G14 ];
+      };
+      "apollo" = nixpkgs.lib.nixosSystem {
+        specialArgs = { inherit inputs; };
+        modules = [ ./nixos/apollo ];
       };
     };
+
+    homeConfigurations = {
+      "bondzula@apollo" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./home-manager/apollo.nix ];
+      };
+
+      "bondzula" = home-manager.lib.homeManagerConfiguration {
+        pkgs = import nixpkgs {
+          system = "x86_64-linux";
+          config.allowUnfree = true;
+        };
+        extraSpecialArgs = { inherit inputs; };
+        modules = [ ./home-manager/g14.nix ];
+      };
+    };
+  };
 }
