8fa7af1a57122ba2c3b3437c8e73b5ef09729016
Stefan Bondzulic <stefanbondzulic@gmail.com>
2023-08-01 13:06:19 +0200

    feat(console): add common console tools

--- home-manager/apollo.nix
+++ home-manager/apollo.nix
@@ -45,24 +45,35 @@
 
   home.packages = with pkgs; [
     wl-clipboard
-    bc
-    curl
-    wget
-    unzip
-    imagemagick
     terraform # TODO: Move to project specific
     awscli2
     ssm-session-manager-plugin
-    httpie
-    jq
-    trash-cli
     nodePackages.pnpm
-    gnumake
-    gcc
     flyctl # TODO: Move to project specific
     supabase-cli # TODO: Move to project specific
     bitwarden
     logseq
+
+    # Console
+    asciinema # Terminal recorder
+    difftastic # Modern Unix `diff`
+    dua # Modern Unix `du`
+    duf # Modern Unix `df`
+    du-dust # Modern Unix `du`
+    fd # Modern Unix `find`
+    glow # Terminal Markdown renderer
+    gping # Modern Unix `ping`
+    hyperfine # Terminal benchmarking
+    moar # Modern Unix `less`
+    neofetch # Terminal system info
+    procs # Modern Unix `ps`
+    tldr # Modern Unix `man`
+    tokei # Modern Unix `wc` for code
+    curl
+    httpie # Modern curl
+    wget
+    unzip
+    trash-cli
   ];
 
   # Set default browser
