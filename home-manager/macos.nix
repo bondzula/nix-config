b98e072e9a8820c97885fdff54734958751eba09
Stefan Bondzulic <stefanbondzulic@gmail.com>
2024-01-01 22:35:55 +0100

    chore(formatt): format macos module

--- home-manager/macos.nix
+++ home-manager/macos.nix
@@ -1,19 +1,32 @@
 { pkgs, outputs, ... }:
 
 {
-  # Home Manager needs a bit of information about you and the paths it should
-  # manage.
-  home.username = "stefanbondzulic";
-  home.homeDirectory = "/Users/stefanbondzulic";
+  home = {
+    # Home Manager needs a bit of information about you and the paths it should
+    # manage.
+    username = "stefanbondzulic";
+    homeDirectory = "/Users/stefanbondzulic";
 
-  # This value determines the Home Manager release that your configuration is
-  # compatible with. This helps avoid breakage when a new Home Manager release
-  # introduces backwards incompatible changes.
-  #
-  # You should not changeg this value, even if you update Home Manager. If you do
-  # want to update the value, then make sure to first check the Home Manager
-  # release notes.
-  home.stateVersion = "23.11";
+    # This value determines the Home Manager release that your configuration is
+    # compatible with. This helps avoid breakage when a new Home Manager release
+    # introduces backwards incompatible changes.
+    #
+    # You should not changeg this value, even if you update Home Manager. If you do
+    # want to update the value, then make sure to first check the Home Manager
+    # release notes.
+    stateVersion = "23.11";
+
+    packages = with pkgs; [
+      fnm
+      mariadb
+      awscli2
+      terraform
+      jq
+      ssm-session-manager-plugin
+      lazygit
+      yazi
+    ];
+  };
 
   # Import existing modules
   imports = [
@@ -24,14 +37,4 @@
 
   # Let Home Manager install and manage itself.
   programs.home-manager.enable = true;
-
-  home.packages = with pkgs; [
-    fnm
-    mariadb
-    awscli2
-    terraform
-    jq
-    ssm-session-manager-plugin
-    lazygit
-  ];
 }
